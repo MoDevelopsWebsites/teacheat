bf348b95253ae9309961de1d15c03ead1bf81677
[dyad] <developerdeyy@gmail.com>
2025-10-09 11:22:43 +0100

    [dyad] Configured app for GitHub Pages by using HashRouter and removing Cloudflare Pages specific files - wrote 1 file(s), deleted 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // Changed BrowserRouter to HashRouter
 import LandingPage from "./pages/LandingPage";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
@@ -24,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter> {/* Changed to HashRouter */}
         <SessionContextProvider>
           <Elements stripe={stripePromise}>
             <Routes>
@@ -40,7 +40,7 @@ const App = () => (
             </Routes>
           </Elements>
         </SessionContextProvider>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
