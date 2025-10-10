b96497dc06a88440c4983b1306b707d400494830
[dyad] <developerdeyy@gmail.com>
2025-10-10 09:30:02 +0100

    [dyad] Restructured Founders page to match Enterprise page layout and content. - wrote 1 file(s)

--- src/pages/Founders.tsx
+++ src/pages/Founders.tsx
@@ -43,11 +43,7 @@ const Founders: React.FC = () => {
         }}
       ></div>
 
-      {/* Floating Background Elements (from previous Founders.tsx) */}
-      <div className="absolute top-1/4 left-1/4 w-48 h-48 bg-blue-300/30 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-float z-0"></div>
-      <div className="absolute bottom-1/3 right-1/4 w-64 h-64 bg-purple-300/30 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-float animation-delay-2000 z-0"></div>
-      <div className="absolute top-1/2 left-1/2 w-56 h-56 bg-pink-300/30 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-float animation-delay-4000 z-0"></div>
-      <div className="absolute bottom-1/4 left-1/3 w-40 h-40 bg-green-300/30 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-float animation-delay-6000 z-0"></div>
+      {/* Removed Floating Background Elements to match EnterpriseLandingPage */}
 
       {/* Header - fixed and conditionally blurred */}
       <Header
