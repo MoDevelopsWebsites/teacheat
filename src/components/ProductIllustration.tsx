4ef43a6b713f5b892e88bf441f8be045f9b6b188
[dyad] <developerdeyy@gmail.com>
2025-11-05 07:53:44 +0000

    [dyad] wrote 1 file(s)

--- src/components/ProductIllustration.tsx
+++ src/components/ProductIllustration.tsx
@@ -10,7 +10,7 @@ const ProductIllustration: React.FC = () => {
   return (
     <div className="relative w-full max-w-full h-[700px] md:h-[800px] lg:h-[900px] flex items-center justify-center p-4">
       {/* Main Meeting Window Mockup (Central, with diagonal angle) */}
-      <div className="absolute z-20 w-full max-w-[900px] h-full max-h-[560px] transform -rotate-3 translate-x-0 translate-y-0">
+      <div className="absolute z-20 w-full max-w-[1000px] h-full max-h-[625px] transform -rotate-3 translate-x-0 translate-y-0">
         <MeetingWindowMockup
           currentAiResponse={currentAiResponse}
         />
