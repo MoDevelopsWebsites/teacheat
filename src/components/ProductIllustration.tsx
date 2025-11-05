12c14ea905a32bad981e5c2f6c3b3f766fa596d5
[dyad] <developerdeyy@gmail.com>
2025-11-05 07:41:31 +0000

    [dyad] wrote 1 file(s)

--- src/components/ProductIllustration.tsx
+++ src/components/ProductIllustration.tsx
@@ -9,8 +9,8 @@ const ProductIllustration: React.FC = () => {
 
   return (
     <div className="relative w-full max-w-7xl h-[600px] md:h-[700px] lg:h-[800px] flex items-center justify-center overflow-hidden p-4">
-      {/* Background gradient matching the black, grey, white scheme */}
-      <div className="absolute inset-0 bg-gradient-to-br from-gray-100 to-white dark:from-gray-900 dark:to-gray-800 opacity-70 rounded-xl"></div>
+      {/* Removed: Background gradient matching the black, grey, white scheme */}
+      {/* <div className="absolute inset-0 bg-gradient-to-br from-gray-100 to-white dark:from-gray-900 dark:to-gray-800 opacity-70 rounded-xl"></div> */}
 
       {/* Main Meeting Window Mockup (Central, with diagonal angle) */}
       <div className="absolute z-20 w-full h-full max-w-[900px] transform -rotate-3 translate-x-0 translate-y-0">
